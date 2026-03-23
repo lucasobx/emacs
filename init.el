f5ddd721af1f16a9598ab2a27aaf41fd0f332360
Lucas Oliveira <obxlucas@gmail.com>
2026-03-23 15:45:36 -0300

    add hook for line-reminder

--- init.el
+++ init.el
@@ -388,6 +388,8 @@
 
 (use-package line-reminder
   :ensure t
+  :hook
+  (prog-mode . line-reminder-mode)
   :config
   (add-hook 'minibuffer-setup-hook (lambda () (line-reminder-mode -1)))
   (setopt line-reminder-show-option 'indicators)
@@ -395,8 +397,7 @@
   (set-face-attribute 'line-reminder-modified-sign-face nil
                       :foreground "#a67c6a")
   (set-face-attribute 'line-reminder-saved-sign-face nil
-                      :foreground "#758672")
-  (global-line-reminder-mode 1))
+                      :foreground "#758672"))
 
 ;;; ===============================================================
 ;;; Navigation

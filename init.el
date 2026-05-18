2011d3099a8ffdb955a0f8b81427ab54ef1411b2
Lucas Oliveira <obxlucas@gmail.com>
2026-05-18 01:31:21 -0300

    remove embark

--- init.el
+++ init.el
@@ -638,55 +638,13 @@
   :bind
   ("C-c c" . consult-dir))
 
-(use-package embark
-  :ensure t
-  :defer t
-  :bind
-  (("C-." . embark-act))
-  :init
-  (setopt prefix-help-command #'embark-prefix-help-command)
-  :config
-  (defun embark-which-key-indicator ()
-    (lambda (&optional keymap targets prefix)
-      (if (null keymap)
-          (which-key--hide-popup-ignore-command)
-        (which-key--show-keymap
-         (if (eq (plist-get (car targets) :type) 'embark-become)
-             "Become"
-           (format "Act on %s '%s'%s"
-                   (plist-get (car targets) :type)
-                   (embark--truncate-target (plist-get (car targets) :target))
-                   (if (cdr targets) "…" "")))
-         (if prefix
-             (pcase (lookup-key keymap prefix 'accept-default)
-               ((and (pred keymapp) km) km)
-               (_ (key-binding prefix 'accept-default)))
-           keymap)
-         nil nil t (lambda (binding)
-                     (not (string-suffix-p "-argument" (cdr binding))))))))
-
-  (defun embark-hide-which-key-indicator (fn &rest args)
-    "Hide the which-key indicator immediately when using the completing-read prompter."
-    (which-key--hide-popup-ignore-command)
-    (let ((embark-indicators
-           (remq #'embark-which-key-indicator embark-indicators)))
-      (apply fn args)))
-
-  (advice-add #'embark-completing-read-prompter
-              :around #'embark-hide-which-key-indicator)
-
-  (setq embark-indicators
-        '(embark-which-key-indicator
-          embark-highlight-indicator
-          embark-isearch-highlight-indicator)))
-
 ;; ===============================================================
 ;;; EDITING
 
 (use-package move-text
   :ensure t
   :bind
-  (("M-<up>"   . move-text-up)
+  (("M-<up>" . move-text-up)
    ("M-<down>" . move-text-down)))
 
 (use-package sudo-edit

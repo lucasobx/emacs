01a2cbe017d03e6a81deff63bc6a16f5e5eee79f
Lucas Oliveira <obxlucas@gmail.com>
2026-06-16 18:31:23 -0300

    add my/exec-path-from-shell

--- init.el
+++ init.el
@@ -183,6 +183,36 @@
                      (lambda (buffer) (diff-buffer-with-file (buffer-file-name buffer)))
                      "show diff between the buffer and its file"))
 
+  (defun my/exec-path-from-shell ()
+    "Sync `exec-path' and PATH with the login shell asynchronously."
+    (interactive)
+    (let* ((shell (file-name-nondirectory (or (getenv "SHELL") "")))
+           (command (pcase shell
+                      ("fish" "fish -c 'string join : $PATH'")
+                      ("bash" "bash --login -c 'printenv PATH'")
+                      ("zsh"  "zsh -i -c 'printenv PATH'")
+                      (_ nil))))
+      (if (not command)
+          (message ">>> exec-path: unsupported shell `%s'" shell)
+        (let ((output ""))
+          (make-process
+           :name "my-exec-path"
+           :buffer nil
+           :noquery t
+           :connection-type 'pipe
+           :command (list shell-file-name shell-command-switch command)
+           :filter (lambda (_proc chunk) (setq output (concat output chunk)))
+           :sentinel
+           (lambda (_proc event)
+             (when (string-prefix-p "finished" event)
+               (let ((path (string-trim output)))
+                 (unless (string-empty-p path)
+                   (setenv "PATH" path)
+                   (setq exec-path (append (parse-colon-path path)
+                                           (list exec-directory)))
+                   (setq-default eshell-path-env path))))))))))
+  (add-hook 'after-init-hook #'my/exec-path-from-shell)
+
   :bind
   ("M-d"       . dired-jump)
   ("M-<right>" . end-of-line)

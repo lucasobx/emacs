38409c70b532ed0e5aa3c7ef5c01e26e745dd48d
Lucas Oliveira <obxlucas@gmail.com>
2026-06-21 15:34:15 -0300

    remove org-remark

--- init.el
+++ init.el
@@ -704,21 +704,6 @@
   :hook
   (org-mode . org-modern-mode))
 
-(use-package org-remark
-  :ensure t
-  :init
-  (org-remark-global-tracking-mode +1)
-  :custom
-  (org-remark-notes-file-name "~/.config/emacs/org/annotations.org")
-  (org-remark-icon-notes nil)
-  :config
-  (with-eval-after-load 'org-remark
-    (setq org-remark-notes-display-buffer-action
-          nil))
-  (org-remark-create "custom1"
-    'mode-line-active
-    '(CATEGORY "custom")))
-
 (use-package org-hide-drawers
   :ensure t
   :custom

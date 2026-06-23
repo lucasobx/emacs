d4d899c01e3f8c39a48cc8acdf8ba4244e1799b2
Lucas Oliveira <obxlucas@gmail.com>
2026-06-23 19:35:39 -0300

    remove unused provide

--- init.el
+++ init.el
@@ -3,13 +3,14 @@
 ;;; Code:
 
 (require 'package)
+
 (setq package-archives
       '(("gnu"    . "https://elpa.gnu.org/packages/")
         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
         ("melpa"  . "https://melpa.org/packages/")))
+
 (setq use-package-vc-prefer-newest t)
 (require 'use-package)
-(provide 'my-bootstrap)
 
 ;; ===============================================================
 ;;; MODULES

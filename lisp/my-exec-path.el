092791d4814526dc9af7c59605b4c97e63c7f651
Lucas Oliveira <obxlucas@gmail.com>
2026-08-08 23:08:00 -0300

    fix PATH parsing issues

--- lisp/my-exec-path.el
+++ lisp/my-exec-path.el
@@ -17,28 +17,41 @@
          (command (my/exec-path--command shell)))
     (if (not command)
         (message ">>> exec-path: unsupported shell `%s'" shell)
-      (let ((output ""))
-        (make-process
-         :name "my-exec-path"
-         :buffer nil
-         :noquery t
-         :connection-type 'pipe
-         :command command
-         :filter (lambda (_proc chunk) (setq output (concat output chunk)))
-         :sentinel
-         (lambda (_proc event)
-           (cond
-            ((string-prefix-p "finished" event)
-             (let ((path (string-trim output)))
-               (if (string-empty-p path)
-                   (lwarn 'my/exec-path :warning "empty PATH from `%s'" shell)
-                 (setenv "PATH" path)
-                 (setq exec-path (append (parse-colon-path path)
-                                         (list exec-directory)))
-                 (setq-default eshell-path-env path))))
-            ((string-match-p "\\`\\(?:exited abnormally\\|failed\\)" event)
-             (lwarn 'my/exec-path :warning
-                    "`%s' failed to report PATH: %s" shell (string-trim event))))))))))
+      (let ((output "")
+            ;; Capture stderr separately and discard it: an interactive shell
+            ;; with no tty prints job-control warnings that would otherwise be
+            ;; mixed into stdout and corrupt the PATH we parse.
+            (stderr (generate-new-buffer " *my-exec-path-stderr*")))
+        ;; `make-process' signals synchronously when the shell is missing;
+        ;; clean up the stderr buffer instead of leaking it in that case.
+        (condition-case err
+            (make-process
+             :name "my-exec-path"
+             :buffer nil
+             :noquery t
+             :connection-type 'pipe
+             :command command
+             :stderr stderr
+             :filter (lambda (_proc chunk) (setq output (concat output chunk)))
+             :sentinel
+             (lambda (_proc event)
+               (unwind-protect
+                   (cond
+                    ((string-prefix-p "finished" event)
+                     (let ((path (string-trim output)))
+                       (if (string-empty-p path)
+                           (lwarn 'my/exec-path :warning "empty PATH from `%s'" shell)
+                         (setenv "PATH" path)
+                         (setq exec-path (append (remq nil (parse-colon-path path))
+                                                 (list exec-directory)))
+                         (setq-default eshell-path-env path))))
+                    ((string-match-p "\\`\\(?:exited abnormally\\|failed\\)" event)
+                     (lwarn 'my/exec-path :warning
+                            "`%s' failed to report PATH: %s" shell (string-trim event))))
+                 (when (buffer-live-p stderr) (kill-buffer stderr)))))
+          (error
+           (when (buffer-live-p stderr) (kill-buffer stderr))
+           (signal (car err) (cdr err))))))))
 
 (add-hook 'after-init-hook #'my/exec-path-from-shell)
 
